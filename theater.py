4954e006df97a69fe4428c1aa14b0eb66fff5531
김주열 <100749830+dufwn1234@users.noreply.github.com>
2023-01-02 17:44:55 +0900

    19번째 연습(1)

---
+++ theater.py
@@ -0,0 +1,27 @@
+##모듈(practice19 와 함께)
+# import practice19
+# practice19.price(3) #3명이서 영화보러 갔을때
+# practice19.price_morning(4)
+# practice19.price_soldier(5)
+
+# import practice19 as pr
+# pr.price(3)
+# pr.price_soldier(5)
+# pr.price_morning(4)
+
+# from practice19 import *
+# price(5)
+# price_morning(4)
+# price_soldier(5)
+
+# from practice19 import price, price_morning #군인 요금 제외
+# price(10)
+# price_morning(10)
+
+# from practice19 import price_soldier as ps #군인만 빼고, 군인 요금을 다른용어로
+# ps(5)
+
+
+
+
+
