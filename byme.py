6f376d998ee66169d743a85c2e62ce8616f8d96f
김주열 <100749830+dufwn1234@users.noreply.github.com>
2023-01-02 17:43:40 +0900

    21번째 연습(1)

---
+++ byme.py
@@ -0,0 +1,12 @@
+def sign():
+    
+    print("이 프로그램은 나도코딩에 의해 만들어 졌습니다.")
+    print("유튜브 : http://youtube.com")
+    print("이메일 : nadocoding@gmail.com")
+
+
+    
+a=map(int,input().split())
+b=map(int,input().split())
+
+print(a,b)
